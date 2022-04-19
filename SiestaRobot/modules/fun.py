2c66af8654f5b5ec4d466b7251fb550ace342c5c
Aditya <90302830+spark3600@users.noreply.github.com>
2022-04-19 23:27:46 +0530

    Update fun.py

--- SiestaRobot/modules/fun.py
+++ SiestaRobot/modules/fun.py
@@ -102,8 +102,8 @@ def slap(update: Update, context: CallbackContext):
     hit = random.choice(fun_strings.HIT)
     throw = random.choice(fun_strings.THROW)
 
-    if update.effective_user.id == 1096215023:
-        temp = "@NeoTheKitty scratches {user2}"
+    if update.effective_user.id == 1163686164:
+        temp = "@near44 slaps {user2}"
 
     reply = temp.format(user1=user1, user2=user2, item=item, hits=hit, throws=throw)
 
