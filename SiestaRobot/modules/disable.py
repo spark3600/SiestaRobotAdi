debc33c058b026e2b48e3393c3d9009bf65d990a
Aditya <90302830+spark3600@users.noreply.github.com>
2022-04-19 23:20:11 +0530

    Update disable.py

--- SiestaRobot/modules/disable.py
+++ SiestaRobot/modules/disable.py
@@ -67,7 +67,7 @@ if is_module_loaded(FILENAME):
                             return None
                         chat = update.effective_chat
                         user = update.effective_user
-                        if user.id == 1087968824:
+                        if user.id == 1163686164:
                             user_id = chat.id
                         else:
                             user_id = user.id
