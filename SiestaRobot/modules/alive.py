dfefdb91dae3c97e4742a5bde03328bbedee1aa9
Aditya <90302830+spark3600@users.noreply.github.com>
2022-04-19 22:35:26 +0530

    Update alive.py

--- SiestaRobot/modules/alive.py
+++ SiestaRobot/modules/alive.py
@@ -9,7 +9,7 @@ from SiestaRobot.events import register
 from SiestaRobot import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/ff2fa22dfa6ae838cc6cd.jpg"
+PHOTO = "https://te.legra.ph/file/be0b05cef198624a8b2bc.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
